08b0e8523f52f21e166f6f8b5c4abbdcf6bfcc8e
sudipal2 <55341569+sudipal2@users.noreply.github.com>
2019-12-14 23:03:43 +0530

    Update dbconnect_test.php

--- dbconnect_test.php
+++ dbconnect_test.php
@@ -1,9 +1,15 @@
 <?php
-$dbhost = "ustom-mysql.gamification.svc.cluster.local";
+/* $dbhost = "ustom-mysql.gamification.svc.cluster.local";
 $dbport = 3306;
 $dbuser = "xxuser";
 $dbpwd = "welcome1";
-$dbname = "sampledb";
+$dbname = "sampledb"; */
+
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbport = getenv("MYSQL_SERVICE_PORT");
+$dbuser = getenv("databaseuser");
+$dbpwd = getenv("databasepassword");
+$dbname = getenv("databasename");
 
 $connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 if ($connection->connect_errno) {
